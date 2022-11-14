927fc277ab4379f894394af5a12a0601a1199596
gfarb <gfarb@github.com>
2022-11-14 13:45:32 -0500

    branch-filter action

---
+++ .github/actions/branch-filter/index.js
@@ -0,0 +1,26 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  const issuePrefix = core.getInput("issuePrefix");
+  let ref = core.getInput("branchName");
+  if (!ref) {
+    ref = github.context.ref;
+  }
+  const branchName = ref.substring(ref.lastIndexOf("/") + 1);
+  console.log(`branchName = ${branchName}`);
+  core.setOutput("branchName", branchName);
+  const re = new RegExp("^" + issuePrefix + "(\\d+)");
+  const prefixMatches = branchName.match(re);
+  let matches = "false";
+  let issueNumber = "";
+  if (prefixMatches) {
+    matches = "true";
+    issueNumber = prefixMatches[1];
+  }
+  core.setOutput("matches", matches);
+  core.setOutput("issueNumber", issueNumber);
+  console.log(`matches = ${matches}; issueNumber = ${issueNumber}`);
+} catch (e) {
+  core.setFailed(e.message);
+}
